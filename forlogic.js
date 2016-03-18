3902e84c75b7fb49a682669e0cd86f09ad883de7
kivancnayci <kivancnayci@hotmail.com>
2016-03-18 10:30:33 +0200

    18.03.2016 classroom exercises

---
+++ forlogic.js
@@ -0,0 +1,13 @@
+var elemID = "#result";
+$().ready(function() {
+    var  top;
+    var  left;
+    for(var i=0; i<10; i++) {
+        left = Math.floor((Math.random() * 1000) + 1);
+        top = Math.floor((Math.random() * 800) + 1);
+        $(elemID).append(
+                "div id='" + i + "' class='b' style='top:"
+                + top + "px; left:" + left + "px;'>" +
+                i + "</div>");
+    }
+    });
